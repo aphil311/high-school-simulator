28552f6f29bc75e8e39ffbeea0df5ae4d5af026d
aphil311 <41242437+aphil311@users.noreply.github.com>
2020-05-04 19:37:02 -0500

    offloaded cards to a different file

---
+++ scripts/cards.js
@@ -0,0 +1,156 @@
+var c1 = {
+  person : "Hot Shot Freshman",
+  description : "Challenges you to 'race' the last 2 blocks to campus after a hard workout",
+  option1 : "Destroy him and teach him some respect", o1t : 10, o1g : 0, o1p : 1,
+  option2 : "Laugh it off and save the effort for the race", o2t : 0, o2g : 0, o2p : -1,
+}
+var c2 = {
+  person : "You",
+  description : "Realize you don't have your running shorts for that afternoons practice.",
+  option1 : "Beg to get out of practice", o1t : 5, o1g : 0, o1p : 0,
+  option2 : "Run intervals in jeans", o2t : -15, o2g : 0, o2p : 0,
+}
+var c3 = {
+  person : "Teammate",
+  description : "Who's your favorite coach?",
+  option1 : "Former NCAA D1 still crushing PRs", o1t : 0, o1g : 0, o1p : 1,
+  option2 : "Former NAIA who was 19th in conference", o2t : 0, o2g : 0, o2p : 0,
+}
+var c4 = {
+  person : "You",
+  description : "You're more proud of:",
+  option1 : "Real life social status", o1t : 0, o1g : -.25, o1p : 1,
+  option2 : "Your witty registered Letsrun handle", o2t : -5, o2g : 0, o2p : -1,
+}
+var c5 = {
+  person : "You",
+  description : "Summer before senior year training:",
+  option1 : "TAKE NO PRISONERS 118 days straight", o1t : -60, o1g : 0, o1p : 0,
+  option2 : "A measured, balance approach", o2t : -40, o2g : 0, o2p : 0,
+}
+var c6 = {
+  person : "Teammate",
+  description : "Oh no! The track's closed.",
+  option1 : "Can't beat the 'crete!", o1t : -30, o1g : 0, o1p : 0,
+  option2 : "Measure out reps on the field", o2t : -20, o2g : 0, o2p : 0,
+}
+var c7 = {
+  person : "Normie Friend",
+  description : "There is a big volleyball game that starts right after practice",
+  option1 : "I've got a lot of homework to do", o1t : 0, o1g : .5, o1p : 0,
+  option2 : "Sounds fun", o2t : 0, o2g : -.25, o2p : 2,
+}
+var c8 = {
+  person : "Coach",
+  description : "You've got a decent chance to win this meet if you sit on the leader and make a move over the last 800.",
+  option1 : "Sit and Kick", o1t : 5, o1g : .25, o1p : 1,
+  option2 : "Take it from the gun", o2t : -10, o2g : 0, o2p : 0,
+}
+var c9 = {
+  person : "Coach",
+  description : "We've got a new freshman joining the team today",
+  option1 : "Hang back and show him the route", o1t : 0, o1g : 0, o1p : -1,
+  option2 : "Establish dominance", o2t : -5, o2g : 0, o2p : 1,
+}
+var c10 = {
+  person : "FloTrack",
+  description : "Flotrack wants to film a workout Wednesday and your coach gives you two workout options.",
+  option1 : "8x1k at threshold", o1t : -15, o1g : 0, o1p : 2,
+  option2 : "60x400 at mile race pace", o2t : -45, o2g : 0, o2p : 3,
+}
+var c11 = {
+  person : "Letsrun",
+  description : "Is " + name + " DONE? He absolutely bombed his last race?",
+  option1 : "Respond anonymously", o1t : 0, o1g : -.25, o1p : 0,
+  option2 : "Shake it off", o2t : 0, o2g : .25, o2p : -1,
+}
+var c12 = {
+  person : "Normie Friend",
+  description : "Hey are you running the Turkey Trot this year?",
+  option1 : "Gobble gobble MF!", o1t : 10, o1g : 0, o1p : 1,
+  option2 : "Nope I've got a big race coming up", o2t : 0, o2g : .25, o2p : -1,
+}
+var c13 = {
+  person : "Teacher",
+  description : "Remember we have an AP review session this Sunday",
+  option1 : "Church of the Sunday long Run", o1t : -5, o1g : -.5, o1p : 0,
+  option2 : "I'll be there", o2t : 5, o2g : .5, o2p : 0,
+}
+var c14 = {
+  person : "Mysterious Coach",
+  description : "Mysterious coach Salberto Alazar offers to coach you",
+  option1 : "Sounds sketchy", o1t : 0, o1g : 0, o1p : 0,
+  option2 : "Sounds legit", o2t : -15, o2g : -.75, o2p : -1,
+}
+var c15 = {
+  person : "Teammate",
+  description : "The biggest social influencer in school just posted a picture of you wearing off brand running shoes from 7th grade",
+  option1 : "Uninstall all social media", o1t : 0, o1g : .5, o1p : 0,
+  option2 : "Threatening Instagram post", o2t : 0, o2g : -.25, o2p : -2,
+}
+var c16 = {
+  person : "Coach",
+  description : "Wow, the sun is really blinding.  Where do I turn for the freeway?",
+  option1 : "Over here", o1t : 0, o1g : -.25, o1p : -1,
+  option2 : "Over there", o2t : 0, o2g : .25, o2p : 1,
+}
+var c17 = {
+  person : "Normie Friend",
+  description : "Hey! I just went for a run to train for soccer and now there's this little crown on my strava",
+  option1 : "Be a good friend and explain what a CR is", o1t : 0, o1g : 0, o1p : 1,
+  option2 : "Segment sniping session", o2t : -5, o2g : 0, o2p : 0,
+}
+var c18 = {
+  person : "Parents",
+  description : "We called the missing person hotline because you were gone so long running",
+  option1 : "Cool", o1t : 0, o1g : -.25, o1p : 0,
+  option2 : "Whatever", o2t : 0, o2g : 0, o2p : -1,
+}
+var c19 = {
+  person : "FloTrack",
+  description : "Posts a video of you throwing up in the middle of a race and makes it a meme",
+  option1 : "Deny it was you", o1t : 0, o1g : -.25, o1p : -1,
+  option2 : "Cry", o2t : 0, o2g : 0, o2p : 0,
+}
+var c20 = {
+  person : "Letsrun",
+  description : "A post gets number 6 on trending that claims you are doping",
+  option1 : "Mail RoJo your urine", o1t : -10, o1g : 0, o1p : 1,
+  option2 : "Make your dad write an angry post", o2t : 5, o2g : -.25, o2p : 0,
+}
+var c21 = {
+  person : "Teammate",
+  description : "Hey what route are we doing?",
+  option1 : "Same one we always do", o1t : -5, o1g : 0, o1p : -1,
+  option2 : "Let's mix it up", o2t : -10, o2g : 0, o2p : 0,
+}
+var c22 = {
+  person : "Parents",
+  description : "We think you're beginning to develop an unhealthy obsession with Eliud Kipchoge",
+  option1 : "You're right... *begins taking apart altar*", o1t : 10, o1g : .5, o1p : 1,
+  option2 : "\"Passion is a choice\" -Kipchoge", o2t : 0, o2g : -.5, o2p : -2,
+}
+var c23 = {
+  person : "Mysterious Coach",
+  description : "Hey man I got this gum that'll give you even more energy than RunGum",
+  option1 : "Sir that is a bag of cocaine", o1t : 0, o1g : .25, o1p : 0,
+  option2 : "I'll have a lifetime supply please", o2t : -35, o2g : -1, o2p : -2,
+}
+var c24 = {
+  person : "Teacher",
+  description : "You've got to stop stretching during class",
+  option1 : "Fitness is a lifestyle", o1t : -5, o1g : 0, o1p : -1,
+  option2 : "Fine.", o2t : 0, o2g : .5, o2p : 0,
+}
+var c25 = {
+  person : "Normie Friend",
+  description : "Dude, why are your shorts so short?",
+  option1 : "Peak athletic performance", o1t : -15, o1g : 0, o1p : -1,
+  option2 : "The ladies", o2t : 0, o2g : 0, o2p : -2,
+}
+var end = {   // Can be placed at any index to end the game
+  person : "Game Over",
+  description : "I'm lazy and haven't written the function to tell you what happens after you graduate based on your stats",
+  option1 : "New Game", o1t : 0, o1g : 0, o1p : 0,
+  option2 : "Quit", o2t : 0, o2g : 0, o2p : 0,
+}
